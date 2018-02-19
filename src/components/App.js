df420cee0b053075715516e5f8654d3223394c3c
dariuszkrol <dariuszkrol@nurtureagency.com>
2018-02-19 20:51:01 +0100

    PropTypes

--- src/components/App.js
+++ src/components/App.js
@@ -4,29 +4,29 @@ import GamesList from "./GamesList";
 const games= [
     {
         _id: 1,
-        price: '32,99',
+        price: 32.99 ,
         thumbnail: 'https://cf.geekdo-images.com/BMUcxCZM_AikQ7uXeuDg43RZIWo=/fit-in/246x300/pic2840020.jpg',
         name: 'quadropolis',
         players: "2-4",
-        duration: "60",
+        duration: 20,
 
     },
     {
         _id: 2,
-        price: '22,99',
+        price: 22.99 ,
         thumbnail: 'https://cf.geekdo-images.com/BMUcxCZM_AikQ7uXeuDg43RZIWo=/fit-in/246x300/pic2840020.jpg',
         name: 'Heroes',
         players: "2",
-        duration: "60",
+        duration: 60,
 
     },
     {
         _id: 3,
-        price: '32,99',
+        price: 32.99 ,
         thumbnail: 'https://cf.geekdo-images.com/BMUcxCZM_AikQ7uXeuDg43RZIWo=/fit-in/246x300/pic2840020.jpg',
         name: 'Red Alert',
         players: "1",
-        duration: "60",
+        duration: 80,
 
     }
 
--- src/components/GamesList.js
+++ src/components/GamesList.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import GameCard from './GameCard';
+import PropTypes from 'prop-types';
 
 const GamesList  = ({games}) => (
     <div className="ui four cards">
         {games.map(game => <GameCard key={game._id} game={game}/>)}
     </div>
 );
-
+GamesList.propTypes = {
+    games: PropTypes.arrayOf(PropTypes.object).isRequired
+};
 export default GamesList;
