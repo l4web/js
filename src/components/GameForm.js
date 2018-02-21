2f210fa73b69a1a30515a31581da86da1ad50446
dariuszkrol <dariuszkrol@nurtureagency.com>
2018-02-21 20:49:22 +0100

    text areafield

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 class GameForm extends React.Component {
     state={
-        name: ''
+        name: '',
+        description: ''
     };
 
     handleSubmit = e => {
@@ -12,8 +13,8 @@ class GameForm extends React.Component {
         })
     };
 
-    handleChangeName = e => {
-        this.setState({ name: e.target.value})
+    handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value})
     };
 
     render(){
@@ -24,10 +25,23 @@ class GameForm extends React.Component {
                     <input
                         type="text"
                         id="name"
+                        name="name"
                         className="text"
                         placeholder="Full game title"
                         value = {this.state.name}
-                        onChange={this.handleChangeName}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div className="field">
+                    <label htmlFor="description">Game description</label>
+                    <textarea
+                        type="text"
+                        id="description"
+                        name="description"
+                        className="text"
+                        placeholder="Game description"
+                        value = {this.state.description}
+                        onChange={this.handleChange}
                     />
                 </div>
                 <button className="button ui" type="submit">
